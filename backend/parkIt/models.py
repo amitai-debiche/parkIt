6bbb5c85d66b62866298b4bee80a3c27ecb08740
Amitai <amivids603@gmail.com>
2023-11-11 15:26:38 -0600

    post-list backend v1

--- backend/parkIt/models.py
+++ backend/parkIt/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-# Creating model/database for posts
+#Creating
+
+class Post(models.Model):
+    # Creator temporarily a name, should change to more unique id per user
+    # CHANGE User later on?
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    location = models.CharField(max_length=300)
+    description = models.TextField(null = True, blank=True)
+    spots = models.IntegerField()
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add = True)
+    # post_id
+    #
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.location)
+    
+   
--- backend/parkIt/views.py
+++ backend/parkIt/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from parkIt.serializers import PostSerializer
+from parkIt.models import Post
+
 
 # Create your views here.
+class GetRoutes(APIView):
+    def get(self, request):
+        routes = [
+            '/api/posts/'
+        ]
+        return Response(routes)
+
+
+
+class PostList(APIView):
+
+    def get(self, request):
+        #search_query = request.GET.get('search') if request.GET.get('search') != None else ''
+
+        posts = Post.objects.all()
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
