2160b95f00d751ffc7b20bc6f2817b2b84abc6b8
Josh Charpentier <charpentier.josh@gmail.com>
2023-11-11 16:10:10 -0600

    merge complete

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -2,41 +2,39 @@ import NavBar from "./components/NavBar.tsx"
 import HomePost from "./components/HomePost.tsx"
 
 import "./index.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+interface Data extends Array<Posts> {}
+
+interface Posts {
+  id: number;
+  location: string;
+  description: string;
+  spots: number;
+  price: number;
+  created: string;
+  creator: number
+}
 
 function Home() {
-	const test_posts = [
-		{
-			creator: 1,
-			location: "Edu Sciences",
-			des: "test woot woot",
-			spots: 1,
-			price: 50,
-			id: 1,
-		},
-		{
-			creator: 2,
-			location: "Frog Lane",
-			des: "test woot woot woot",
-			spots: 4,
-			price: 500,
-			id: 2,
-		},
-		{
-			creator: 3,
-			location: "Ingraham",
-			des: "test woot ayyy woot",
-			spots: 10,
-			price: 1000,
-			id: 3,
-		},
-	]
+  const [data, setData] = useState<Data>([]);
+
+  useEffect(() => {
+    // Replace 'api_url' with the actual URL of your Django API endpoint
+    fetch('http://127.0.0.1:8000/api/posts/')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   const [search, setSearch] = useState('')
-
+  console.log(data)
   function searchPosts(newSearch: string) {
     setSearch(newSearch)
-    console.log(newSearch)
   }
 
 	return (
@@ -44,7 +42,7 @@ function Home() {
 			<NavBar search={searchPosts}/>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				{test_posts.filter(post => post.location.toLowerCase().includes(search.trim().toLowerCase())).map((post) => {
+				{data.filter(post => post.location.toLowerCase().includes(search.trim().toLowerCase())).map((post) => {
 					return (
 						<HomePost
 							location={post.location}
