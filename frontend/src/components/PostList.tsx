4d2bbef80f241646cca4df59714ac565e35b8937
Amitai <amivids603@gmail.com>
2023-11-12 05:28:05 -0600

    checking CHANGES SHOULD RETURN IF BAD

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -15,41 +15,48 @@ export interface Posts {
 	price: number
 	created: string
 	creator: number
+	images: string
 }
 
 const PostList: React.FC<Props> = ({ posts, likedPosts, onToggleLike }) => {
 	return (
-		<div className="home-grid">
-			{posts.map((post) => (
-				<div key={post.id} className="home-post-container">
-					<div className="hover:cursor-pointer relative">
-						<div className="heart-icon">
-							<button
-								type="button"
-								className={`nav-button z-10 absolute top-2 right-2 ${
-									likedPosts.includes(post.id)
-										? "text-red-500 active:text-red-300 hover:text-red-500"
-										: "hover:text-red-500 text-gray-500"
-								}`}
-								onClick={() => {
-									onToggleLike(post.id)
-								}}
-							>
-								<HeartIcon className="h-6 w-6" aria-hidden="true" />
-							</button>
-						</div>
-						<HomePost
-							location={post.location}
-							spots={post.spots}
-							price={post.price}
-							creator={post.creator}
-							id={post.id}
-						/>
-					</div>
-				</div>
-			))}
-		</div>
-	)
-}
-
-export default PostList
+	  <div className="home-grid">
+		{posts.map((post) => (
+		  <div key={post.id} className="home-post-container">
+			<div className="hover:cursor-pointer relative">
+			  <div className="heart-icon">
+				<button
+				  type="button"
+				  className={`nav-button z-10 absolute top-2 right-2 ${
+					likedPosts.includes(post.id)
+					  ? "text-red-500 active:text-red-300 hover:text-red-500"
+					  : "hover:text-red-500 text-gray-500"
+				  }`}
+				  onClick={() => {
+					onToggleLike(post.id);
+				  }}
+				>
+				  <HeartIcon className="h-6 w-6" aria-hidden="true" />
+				</button>
+			  </div>
+			  {/* Add an image element here */}
+			  <img
+				src={post.images} // Assuming `image` is the URL to the image
+				alt={`Image for ${post.description}`}
+				className="w-full h-40 object-cover rounded-t-md"
+			  />
+			  <HomePost
+				location={post.location}
+				spots={post.spots}
+				price={post.price}
+				creator={post.creator}
+				id={post.id}
+			  />
+			</div>
+		  </div>
+		))}
+	  </div>
+	);
+  };
+  
+  export default PostList;
